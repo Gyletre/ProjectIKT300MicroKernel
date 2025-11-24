973282642261e66628a03fa2caf1280b8b971fbf
Elias Kristoffer Grønn Moen <ekmoen@uia.no>
2025-11-24 11:45:44 +0100

    Created Skeleton of solution

---
+++ MicroKernel/Program.cs
@@ -0,0 +1,16 @@
+
+using System;
+using System.Diagnostics;
+
+
+public class MicroKernel
+{
+    private static void Main()
+    {
+        Console.WriteLine("Greetings, Universe");
+    }
+}
+
+
+
+
