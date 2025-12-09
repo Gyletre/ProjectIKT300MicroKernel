e4b0c73f00146b1c1fba403ad64354dc9987631e
Elias Kristoffer Grønn Moen <ekmoen@uia.no>
2025-12-09 18:15:17 +0100

    Delete heartbeat monitor

--- micro_kernel/classes/abstract_plugin.py
+++ micro_kernel/classes/abstract_plugin.py
@@ -1,20 +1,10 @@
 from abc import ABC, abstractmethod
 from classes.i_message_client import IMessageClient
-from time import time
-from asyncio import run
 class AbstractPlugin(IMessageClient):
     def __init__(self,id,port):
         self.id = id
         self.ConnectToBroker(port)
-        run(self.HeartBeat)
         super().__init__()
-
-    def HeartBeat(self):
-        print("heartbeat")
-        startTime=time()
-        while(time()<startTime+10):
-            pass
-        self.HeartBeat()
 
     def ShutDown(self):
         del(self)
