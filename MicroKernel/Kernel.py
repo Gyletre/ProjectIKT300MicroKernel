76cc48fbe8e6dbc248c86632e85bfdab30492a1e
Nils August Jakobsen <nilsaj@uia.no>
2025-12-09 17:28:42 +0100

    byttet til python

---
+++ MicroKernel/Kernel.py
@@ -0,0 +1,60 @@
+import time
+
+class Kernel:
+    def __init__(self):
+        self.running = True
+        self.plugins = []
+        self.fps = 60
+        self.launcher = None
+
+    def set_launcher(self, launcher):
+        self.launcher = launcher
+
+    def add_plugin(self, plugin):
+        self.plugins.append(plugin)
+
+    def restart_plugin(self, plugin):
+        plugin.stop()
+        plugin.start()
+
+    def run(self):
+        print("Greetings, Universe")
+        
+        if self.launcher:
+            self.launcher.run_plugins(self)
+
+        for plugin in self.plugins:
+            plugin.start()
+
+        old_time = time.time()
+        time_to_run = 300
+
+        while self.running:
+            current_time = time.time()
+            
+            # Enkel pause for å ikke bruke 100% CPU
+            time.sleep(1/self.fps)
+
+            for i in range(len(self.plugins) - 1, -1, -1):
+                plugin = self.plugins[i]
+                if plugin.heart_monitor() == 1:
+                    plugin.process()
+                else:
+                    print("Plugin virket ikke")
+                    self.restart_plugin(plugin)
+                    if plugin.heart_monitor() == 1:
+                        print("Plugin restartet")
+                    else:
+                        plugin.stop()
+                        self.plugins.pop(i)
+            
+            time_to_run -= 1
+            if time_to_run < 0:
+                self.running = False
+        
+        self.shutdown()
+
+    def shutdown(self):
+        for plugin in self.plugins:
+            plugin.stop()
+        self.plugins.clear()
--- MicroKernel/Scripts/Launcher/ILauncher.cs
+++
@@ -1,4 +0,0 @@
-namespace IKT300.Launcher;
-
-public interface ILauncher{
-    void RunPlugins(Kernel kernel);}
