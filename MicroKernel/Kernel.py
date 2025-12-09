51ae82e7d4322476652fdf0703c79dfcb6eb177b
Nilsjakobsen <nils.a.jakobsen@gmail.com>
2025-12-09 18:23:17 +0100

    Fix formatting issues in Kernel.py

--- MicroKernel/Kernel.py
+++ MicroKernel/Kernel.py
@@ -20,7 +20,6 @@ class Kernel:
         
         if self.launcher:
             self.launcher.run_plugins(self)
-
         for plugin in self.plugins:
             plugin.start()
 
@@ -29,24 +28,23 @@ class Kernel:
 
         while self.running:
             current_time = time.time()
-            
             time.sleep(1/self.fps)
 
-            for i in range(len(self.plugins) - 1, -1, -1):
+            for i in range(len(self.plugins) -  1, -1, -1):
                 plugin = self.plugins[i]
                 if plugin.heart_monitor() == 1:
                     plugin.process()
                 else:
                     print("Plugin didnt work")
                     self.restart_plugin(plugin)
-                    if plugin.heart_monitor() ==  1:
+                    if plugin.heart_monitor() ==   1:
                         print("Plugin restarted")
                     else:
                         plugin.stop()
                         self.plugins.pop(i)
             
             time_to_run -= 1
-            if time_to_run < 0:
+            if time_to_run <  0:
                 self.running =  False
         
         self.shutdown()
