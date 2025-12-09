ccae4664b74a5f47172c3de9911bf72cecad727e
Elias Kristoffer Grønn Moen <ekmoen@uia.no>
2025-12-09 19:23:03 +0100

    part 2

--- micro_kernel/classes/abstract_plugin.py
+++ micro_kernel/classes/abstract_plugin.py
@@ -2,15 +2,13 @@ from abc import ABC, abstractmethod
 from classes.i_message_client import IMessageClient
 from time import time
 from asyncio import run
+from classes.mqtt_client import MQTTClient
 
-class AbstractPlugin():
-    def __init__(self,id,brokerIP,port):
-        self.id = id
-        self.brokerIP = brokerIP
-        self.client = IMessageClient()
-        # connect to broker
-        run(self.HeartBeat)
-        super().__init__()
+class AbstractPlugin(MQTTClient):
+    """Subscribe to topics with self._Subscribe(topic). \n
+    Then override self._OnDataRecieved() and self.SendData() to add plugin specific implementation"""
+    def __init__(self, config, on_message):
+        super().__init__(config, on_message)
 
     def ShutDown(self):
         del(self)
--- micro_kernel/classes/kernel.py
+++ micro_kernel/classes/kernel.py
@@ -1,23 +1,19 @@
 from classes.i_launcher import ILauncher
 from classes.i_message_client import IMessageClient
-class Kernel(IMessageClient):
-    def __init__(self, l: ILauncher,port:int):
+from classes.mqtt_client import MQTTClient,MQTTClientConfig
+class Kernel(MQTTClient):
+    def __init__(self, l: ILauncher,brokerIP,port:int):
         self.launcher: ILauncher = l
         self.__Startup(port)
-    def __Startup(self,port):
-        self.ConnectToBroker(port)
+        super().__init__(MQTTClientConfig(-1,brokerIP,port))
+    def __Startup(self):
+        self._ConnectToBroker()
         self.pluginIDs = self.launcher.RunPlugins()
     def ShutDownAll(self):
-        # Send shut down message to all plugins
+        self._SendData("status/shutdown",'\{"program":1\}')
+
         pass
     def __del__(self):
         self.ShutDownAll()
-
-    def SendData(self, topic, msg):
-        return super().SendData(topic, msg)
-    def OnDataRecieved(self, msg):
-        return super().OnDataRecieved(msg)
-    def ConnectToBroker(self, port):
-        return super().ConnectToBroker(port)
 
         
